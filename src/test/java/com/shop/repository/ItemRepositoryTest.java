364a3b4eade44dbbe86adab79dda45c8e2d8d900
khs <69028828+happysang@users.noreply.github.com>
2022-06-23 02:56:24 +0900

    .

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -1,6 +1,9 @@
 package com.shop.repository;
 
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.Entity.Item;
+import com.shop.Entity.QItem;
 import com.shop.constant.ItemSellStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,12 +11,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
 class ItemRepositoryTest {
+
+    @PersistenceContext
+    EntityManager em;
+
     @Autowired
     ItemRepository itemRepository;
 
@@ -36,7 +45,7 @@ class ItemRepositoryTest {
         for(int i=1; i<=10; i++){
             Item item = new Item();
             item.setItemNm("test item"+i);
-            item.setPrice(10000+1);
+            item.setPrice(10000+i);
             item.setItemDetail("test item detail information"+i);
             item.setItemSellStatus(ItemSellStatus.SELL);
             item.setStockNumber(100);
@@ -68,6 +77,59 @@ class ItemRepositoryTest {
 
     @Test
     @DisplayName("price lessthan test")
-    public
+    public void findByPriceLessThanTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThan(10005);
+        for (Item item:itemList){
+            System.out.println("item.toString() = " + item.toString());
+        }
+    }
 
+    @Test
+    @DisplayName("price lessthan test")
+    public void findByPriceLessThanTestDesc(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10005);
+        for (Item item:itemList){
+            System.out.println("item.toString() = " + item.toString());
+        }
+    }
+    
+    @Test
+    @DisplayName("@Quary read itemList test")
+    public void findByItemDetailTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemDetail("detail information");
+        for (Item item:itemList){
+            System.out.println("item.toString() = " + item.toString());
+        }
+    }
+    
+    @Test
+    @DisplayName("NativeQuary read itemList test")
+    public void findByItemDetailByNative(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemDetailByNative("detail information");
+        for (Item item : itemList){
+            System.out.println("item.toString() = " + item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("Querydsl read test1")
+    public void queryDslTest(){
+        this.createItemList();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QItem qItem = QItem.item;
+        JPAQuery<Item> query = queryFactory.selectFrom(qItem)
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemDetail.like("%"+"detail information"+"%"))
+                .orderBy(qItem.price.desc());
+
+        List<Item> itemList = query.fetch();
+
+        for (Item item : itemList){
+            System.out.println("item.toString() = " + item.toString());
+        }
+    }
 }
