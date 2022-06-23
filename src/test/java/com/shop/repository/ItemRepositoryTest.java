0c33418c339934d2fa89f6d542a0b9789270d9cd
khs <69028828+happysang@users.noreply.github.com>
2022-06-23 18:38:07 +0900

    2장완료

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.shop.repository;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.Entity.Item;
@@ -9,12 +10,18 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.TestPropertySource;
+import org.thymeleaf.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
+
+import static com.shop.Entity.QItem.item;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
@@ -25,6 +32,46 @@ class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    public void createItemList(){
+        for(int i=1; i<=10; i++){
+            Item item = new Item();
+            item.setItemNm("test item"+i);
+            item.setPrice(10000+i);
+            item.setItemDetail("test item detail information"+i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            itemRepository.save(item);
+        }
+    }
+
+    public void createItemList2(){
+        for (int i=1; i<=5; i++){
+            Item item = new Item();
+            item.setItemNm("test item"+i);
+            item.setPrice(10000+i);
+            item.setItemDetail("test item detail information"+i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            itemRepository.save(item);
+        }
+
+        for (int i=6; i<=10; i++){
+            Item item = new Item();
+            item.setItemNm("test item"+i);
+            item.setPrice(10000+i);
+            item.setItemDetail("test item detail information"+i);
+            item.setItemSellStatus(ItemSellStatus.SOLD_OUT);
+            item.setStockNumber(0);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            itemRepository.save(item);
+        }
+    }
 
     @Test
     @DisplayName("item save test")
@@ -39,20 +86,6 @@ class ItemRepositoryTest {
         item.setUpdateTime(LocalDateTime.now());
         Item saveItem = itemRepository.save(item);
         System.out.println("saveItem.toString() = " + saveItem.toString());
-    }
-
-    public void createItemList(){
-        for(int i=1; i<=10; i++){
-            Item item = new Item();
-            item.setItemNm("test item"+i);
-            item.setPrice(10000+i);
-            item.setItemDetail("test item detail information"+i);
-            item.setItemSellStatus(ItemSellStatus.SELL);
-            item.setStockNumber(100);
-            item.setRegTime(LocalDateTime.now());
-            item.setUpdateTime(LocalDateTime.now());
-            itemRepository.save(item);
-        }
     }
 
     @Test
@@ -120,7 +153,7 @@ class ItemRepositoryTest {
     public void queryDslTest(){
         this.createItemList();
         JPAQueryFactory queryFactory = new JPAQueryFactory(em);
-        QItem qItem = QItem.item;
+        QItem qItem = item;
         JPAQuery<Item> query = queryFactory.selectFrom(qItem)
                 .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
                 .where(qItem.itemDetail.like("%"+"detail information"+"%"))
@@ -130,6 +163,34 @@ class ItemRepositoryTest {
 
         for (Item item : itemList){
             System.out.println("item.toString() = " + item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("Querydsl read test2")
+    public void queryDslTest2(){
+        this.createItemList2();
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QItem item = QItem.item;
+
+        String itemDetail = "detail information";
+        int price = 10003;
+        String itemSellStat = "SELL";
+
+        booleanBuilder.and(item.itemDetail.like("%"+itemDetail+"%"));
+        booleanBuilder.and(item.price.gt(price));
+
+        if (StringUtils.equals(itemSellStat, ItemSellStatus.SELL)){
+            booleanBuilder.and(item.itemSellStatus.eq(ItemSellStatus.SELL));
+        }
+
+        Pageable pageable = PageRequest.of(0,5);
+        Page<Item> itemPagingResult = itemRepository.findAll(booleanBuilder,pageable);
+        System.out.println("total elements : " + itemPagingResult.getTotalElements());
+
+        List<Item> resultItemList = itemPagingResult.getContent();
+        for (Item resultItem : resultItemList){
+            System.out.println("결과 : " + resultItem.toString());
         }
     }
 }
