8b2e1611cc72d633330954a6d236d22ffeea65a7
khs <69028828+happysang@users.noreply.github.com>
2022-07-01 01:39:00 +0900

    .
    .

---
+++ src/test/java/com/shop/repository/CartRepositoryTest.java
@@ -0,0 +1,61 @@
+package com.shop.repository;
+
+import com.shop.Entity.Cart;
+import com.shop.Entity.Member;
+import com.shop.dto.MemberFormDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
+class CartRepositoryTest {
+    @Autowired
+    CartRepository cartRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @PersistenceContext
+    EntityManager em;
+
+    public Member createMember(){
+        MemberFormDto memberFormDto = new MemberFormDto();
+        memberFormDto.setEmail("test@test.com");
+        memberFormDto.setName("khs");
+        memberFormDto.setAddress("aa bb cc");
+        memberFormDto.setPassword("1234");
+        return Member.createMember(memberFormDto, passwordEncoder);
+    }
+
+    @Test
+    @DisplayName("cart entity mapping test")
+    public void findCartAndMemberTest(){
+        Member member = createMember();
+        memberRepository.save(member);
+
+        Cart cart = new Cart();
+        cart.setMember(member);
+        cartRepository.save(cart);
+
+        em.flush();
+        em.clear();
+
+        Cart savedCart = cartRepository.findById(cart.getId()).orElseThrow(EntityNotFoundException::new);
+        assertEquals(savedCart.getMember().getId(), member.getId());
+    }
+}
